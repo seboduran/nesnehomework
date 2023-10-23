5a36c64dbb16f991baf657c7763c4b0b637e5073
GÜNCEL SARIMAN <guncelsariman@mu.edu.tr>
2023-10-23 10:44:13 +0300

    Add project files.

---
+++ NesneHafta3/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace NesneHafta3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(255, 192, 128);
+            groupBox1.Location = new Point(12, 71);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(225, 140);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = Color.FromArgb(255, 192, 128);
+            groupBox2.Location = new Point(548, 289);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(225, 140);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(257, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(263, 36);
+            label1.TabIndex = 2;
+            label1.Text = "Hafta 3'e Hoşgeldiniz.";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label1;
+    }
+}
---
+++ NesneHafta3/Form1.cs
@@ -0,0 +1,12 @@
+namespace NesneHafta3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
