999b2d34e4cfb3d5a0b2fa9c4f96304dce0a0ff9
GÜNCEL SARIMAN <guncelsariman@mu.edu.tr>
2023-10-23 11:38:34 +0300

    listeler

--- NesneHafta3/Form1.Designer.cs
+++ NesneHafta3/Form1.Designer.cs
@@ -29,31 +29,152 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            label1 = new Label();
             button1 = new Button();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            button3 = new Button();
             button2 = new Button();
+            comboBox1 = new ComboBox();
+            listBox1 = new ListBox();
+            label1 = new Label();
+            textBox4 = new TextBox();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
             groupBox1.BackColor = Color.FromArgb(255, 192, 128);
-            groupBox1.Location = new Point(12, 71);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(32, 79);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(225, 140);
+            groupBox1.Size = new Size(335, 250);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            groupBox1.Text = "Öğrenci Kayıt Ekranı";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(144, 140);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 68);
+            button1.TabIndex = 3;
+            button1.Text = "Öğrenci Ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(144, 92);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(172, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(144, 60);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(172, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(144, 24);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(172, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(19, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Bölüm Adı";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Öğrenci No";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Ad-Soyad";
             // 
             // groupBox2
             // 
             groupBox2.BackColor = Color.FromArgb(255, 192, 128);
-            groupBox2.Location = new Point(548, 289);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Controls.Add(listBox1);
+            groupBox2.Location = new Point(407, 79);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(225, 140);
+            groupBox2.Size = new Size(328, 359);
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
-            groupBox2.Text = "groupBox2";
+            groupBox2.Text = "Öğrenci Bilgileri";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(85, 14);
+            button3.Name = "button3";
+            button3.Size = new Size(128, 25);
+            button3.TabIndex = 7;
+            button3.Text = "Aktar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(182, 219);
+            button2.Name = "button2";
+            button2.Size = new Size(128, 25);
+            button2.TabIndex = 6;
+            button2.Text = "Temizle";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(18, 190);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(292, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(18, 45);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(292, 139);
+            listBox1.TabIndex = 0;
             // 
             // label1
             // 
@@ -65,24 +186,22 @@
             label1.TabIndex = 2;
             label1.Text = "Hafta 3'e Hoşgeldiniz.";
             // 
-            // button1
+            // textBox4
             // 
-            button1.Location = new Point(32, 19);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 3;
-            button1.Text = "Tıkla";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            textBox4.Location = new Point(29, 277);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(113, 23);
+            textBox4.TabIndex = 8;
             // 
-            // button2
+            // button4
             // 
-            button2.Location = new Point(579, 45);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 4;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            button4.Location = new Point(182, 275);
+            button4.Name = "button4";
+            button4.Size = new Size(128, 25);
+            button4.TabIndex = 9;
+            button4.Text = "Ara";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // Form1
             // 
@@ -90,13 +209,15 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Controls.Add(label1);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -107,6 +228,17 @@
         private GroupBox groupBox2;
         private Label label1;
         private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private ListBox listBox1;
+        private ComboBox comboBox1;
         private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox4;
     }
 }
--- NesneHafta3/Form1.cs
+++ NesneHafta3/Form1.cs
@@ -7,10 +7,43 @@ namespace NesneHafta3
             InitializeComponent();
         }
 
+        List<string> Liste_AdSoyad = new List<string>();
+        List<string> Liste_OgrenciNo = new List<string>();
+        List<string> Liste_BolumAdi = new List<string>();
+        int sayac = 0;
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = "Merhaba Butona týkladým sonunda.";
-            label1.Text += "Naber";
+            //string deger = textBox1.Text + " " + textBox2.Text + " " + textBox3.Text;
+            //listBox1.Items.Add(deger);
+            //comboBox1.Items.Add(deger);
+
+            Liste_AdSoyad.Add(textBox1.Text);
+            Liste_OgrenciNo.Add(textBox2.Text);
+            Liste_BolumAdi.Add(textBox3.Text);
+            sayac++;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            comboBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sayac; i++)
+            {
+                string deger = Liste_AdSoyad[i] + " " + Liste_OgrenciNo[i] + " " + Liste_BolumAdi[i];
+                listBox1.Items.Add(deger);
+                comboBox1.Items.Add(deger);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sonuc = Liste_AdSoyad.Where(ayse => ayse.Contains(textBox4.Text)).Count();
+            MessageBox.Show(textBox4.Text + "de aradýðýn isim listede " + sonuc + " kadar bulunmaktadýr");
         }
     }
 }
